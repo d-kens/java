6a0ebe6ed6d0153d4e21b1e494910cada2f595ab
d-kens <omondikens1@gmail.com>
2024-09-16 00:08:35 +0300

    fizzbuzz

--- src/fundamentals/Main.java
+++ src/fundamentals/Main.java
@@ -1,10 +1,34 @@
 package fundamentals;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+
+        // Comparison operators
+        int x = 2;
+        int y = 3;
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+
+        // If statement
+        int temp = 32;
+
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
+        else if (temp > 20)
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
+
+        int income = 120_000;
+
+        boolean hasHighIncome = (income > 100_000);
 
     }
 }
