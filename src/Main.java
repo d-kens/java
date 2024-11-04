2a0e915301828328d269ba55ad9365c15f2ed979
d-kens <omondikens1@gmail.com>
2024-11-04 05:36:32 +0300

    simple java program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Welcome to JAVA");
+        System.out.println("Programming is fun");
+        System.out.println("Fundamentals first");
+        System.out.println("Problem Driven");
     }
 }
