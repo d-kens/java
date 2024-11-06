02ea87d93359863638a53abfea445c3429649a42
d-kens <omondikens1@gmail.com>
2024-11-06 05:32:32 +0300

    compute area

---
+++ src/fundamentals/CalculateArea.java
@@ -0,0 +1,13 @@
+package fundamentals;
+
+public class CalculateArea {
+    public static void main(String [] args) {
+        double radius;
+        double area;
+
+        radius = 20;
+        area = radius * radius * 3.14159;
+
+        System.out.println("The area of a circle with radius: " + radius + " is: " + area);
+    }
+}
