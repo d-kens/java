1184cf8f014fe3b7b698ee82f9bf06caac80d188
d-kens <omondikens1@gmail.com>
2024-11-15 04:47:21 +0300

    compute body mass index

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-       final double KILOMETERS_PER_MILE = 1.609;
-       double miles = 100;
+       int number = (int)(Math.random() * 41) + 10;
 
-       double kilometers = KILOMETERS_PER_MILE * miles;
-
-       System.out.println(kilometers);
-
-       System.out.println(Math.pow(2, 3));
+       System.out.println("Random Number: " + number);
     }
 }
