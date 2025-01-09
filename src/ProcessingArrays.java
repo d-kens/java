f7c7923f806a29d0a207af57722af3a4545d7928
d-kens <omondikens1@gmail.com>
2025-01-09 15:57:44 +0300

    Implement method for sg=hifting an array

--- src/ProcessingArrays.java
+++ src/ProcessingArrays.java
@@ -2,34 +2,25 @@ public class ProcessingArrays {
     public static void main(String[] args) {
         int[] numbers = {1, 10, 41, 91, 39, 23, 91, 84, 29};
 
-        int max = numbers[0];
-        int indexOfMax = 0;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-                indexOfMax = i;
-            }
-        }
-
-        System.out.println("The maximum number in the array is: " + max + " and its smallest index is: " + indexOfMax);
-
-
-        int temp = numbers[numbers.length - 1];
-
-        for (int i = numbers.length - 1;  i > 0; i--) {
-            numbers[i] = numbers[i - 1];
-        }
-
-        numbers[0] = temp;
-
-
+        numbers = shift(numbers);
 
         for (int number : numbers) System.out.print(number + " ");
     }
 
 
     public static int[] shift(int[] numbers) {
-        return new int[numbers.length];
+        int[] result = new int[numbers.length];
+
+        System.arraycopy(numbers, 0, result, 0, numbers.length);
+
+        int temp = numbers[numbers.length - 1];
+
+        for (int i = result.length - 1; i > 0; i--) {
+            result[i] = result[i - 1];
+        }
+
+        result[0] = temp;
+
+        return result;
     }
 }
