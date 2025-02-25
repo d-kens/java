5c66f2fa55e66d708a353309c6470b6be339af7e
d-kens <omondikens1@gmail.com>
2025-02-25 05:42:47 +0300

    Sum elements in a @D array by column

--- src/fundamentals/Processing2DArrays.java
+++ src/fundamentals/Processing2DArrays.java
@@ -11,6 +11,17 @@ public class Processing2DArrays {
         System.out.println("Matrix values:");
         printMatrix(matrix);
 
+        System.out.println();
+        int[][] matrix2 = initializingWithRandomValues();
+        printMatrix(matrix2);
+
+        System.out.println();
+        int matrix2Sum = sumMatrix(matrix2);
+        System.out.println(matrix2Sum);
+
+        System.out.println();
+        sumColumns(matrix);
+
     }
 
 
@@ -31,11 +42,45 @@ public class Processing2DArrays {
         return matrix;
     }
 
+    public static int[][] initializingWithRandomValues() {
+        int[][] matrix = new int[10][10];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = (int)(Math.random() * 100);
+            }
+        }
+
+        return matrix;
+    }
+
+    public static int sumMatrix(int[][] matrix) {
+        int sum = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for(int column = 0; column < matrix[row].length; column++) {
+                sum += matrix[row][column];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void sumColumns(int[][] matrix) {
+        for (int column = 0; column < matrix[0].length; column++) {
+            int colSum = 0;
+
+            for(int row = 0; row < matrix.length; row++)
+                colSum += matrix[row][column];
+
+            System.out.println("The total sum for column " + column + " is: " + colSum);
+        }
+    }
+
 
     public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.print(value + " ");
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                System.out.print(matrix[row][column] + " ");
             }
             System.out.println();
         }
