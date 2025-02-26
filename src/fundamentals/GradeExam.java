9052d57ba7ebb92c7246b7b7324cf77c54091c57
d-kens <omondikens1@gmail.com>
2025-02-26 05:28:16 +0300

    grade exam

---
+++ src/fundamentals/GradeExam.java
@@ -0,0 +1,39 @@
+package fundamentals;
+
+public class GradeExam {
+
+    public static void main(String[] args) {
+
+        char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+
+
+        // Key to the questions
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+        grade(answers, keys);
+
+    }
+
+
+    public static void grade(char[][] answers, char[] keys) {
+
+        for (int row = 0; row < answers.length; row++) {
+
+            int correctCount = 0;
+            for (int col = 0; col < answers[row].length; col++) {
+                if(answers[row][col] == keys[col]) correctCount++;
+            }
+
+            System.out.println("Student " + row + "'s correct count is " + correctCount);
+        }
+
+    }
+}
