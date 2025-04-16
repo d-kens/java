10cd5113fc8be597ff1e87dabd080bb83b2bfdaf
d-kens <omondikens1@gmail.com>
2025-04-16 05:19:59 +0300

    Costructor chaining and super keyword

---
+++ src/inheritance_polymorphism/Person.java
@@ -0,0 +1,11 @@
+package inheritance_polymorphism;
+
+public class Person {
+    public Person() {
+        System.out.println("(1) Performs Person's tasks");
+    }
+
+    public Person(String s) {
+        System.out.println(s);
+    }
+}
