03d604726f9a6b2cf4ba45a2d44a82800b4b124b
d-kens <omondikens1@gmail.com>
2025-05-06 21:57:02 +0300

    chained exceptionms and custom exceptions

--- src/exception_handling/Circle.java
+++ src/exception_handling/Circle.java
@@ -2,16 +2,15 @@ package exception_handling;
 
 public class Circle {
 
-
     private double radius;
 
     private static int numberOfObjects = 0;
 
-    public Circle() {
+    public Circle() throws InvalidRadiusException {
         this(1.0);
     }
 
-    public Circle(double radius) {
+    public Circle(double radius) throws InvalidRadiusException  {
         setRadius(radius);
         numberOfObjects++;
     }
@@ -20,11 +19,11 @@ public class Circle {
         return radius;
     }
 
-    public void setRadius(double radius) throws IllegalArgumentException {
+    public void setRadius(double radius) throws InvalidRadiusException {
         if (radius >= 0)
             this.radius = radius;
         else
-            throw new IllegalArgumentException("Radius cannot be negative");
+            throw new InvalidRadiusException(radius);
 
     }
 
@@ -40,15 +39,37 @@ public class Circle {
 
     public static void main(String[] args) {
 
+//        try {
+//            Circle circle1 = new Circle(5);
+//            Circle circle2 = new Circle(-5);
+//            Circle circle3 = new Circle(0);
+//        } catch (InvalidRadiusException ex) {
+//            System.out.println(ex);
+//        }
+//
+//        System.out.println("Number of objects created: " + Circle.getNumberOfObjects());
+
         try {
-            Circle circle1 = new Circle(5);
-            Circle circle2 = new Circle(-5);
-            Circle circle3 = new Circle(0);
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex);
+            method();
+            System.out.println("After the method call");
+        } catch (RuntimeException ex) {
+            System.out.println("RuntimeException in main");
+        } catch (Exception ex) {
+            System.out.println("Exception in main.");
+        }
+    }
+
+    public static void method() throws Exception {
+        try {
+            Circle c1 = new Circle(1);
+            c1.setRadius(-1);
+            System.out.println(c1.getRadius());
+        } catch (RuntimeException ex) {
+            System.out.println("RuntimeException in method()");
+        } catch (Exception ex) {
+            System.out.println("Exception in method()");
+            throw ex;
         }
 
-
-        System.out.println("Number of objects created: " + Circle.getNumberOfObjects());
     }
 }
