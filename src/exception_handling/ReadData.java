02bd8e78e1e67b12abe3bf291f5f7d10a0ebd3b0
d-kens <omondikens1@gmail.com>
2025-05-14 22:30:28 +0300

    Covert a program from end of line brace to next of line brace

--- src/exception_handling/ReadData.java
+++ src/exception_handling/ReadData.java
@@ -3,14 +3,18 @@ package exception_handling;
 import java.io.File;
 import java.util.Scanner;
 
-public class ReadData {
-    public static void main(String[] args) throws Exception {
+public class ReadData
+{
+    public static void main(String[] args) throws Exception
+    {
         // create a file
         File file = new File("score.txt");
 
 
-        try (Scanner input = new Scanner(file);) {
-            while (input.hasNext()) {
+        try (Scanner input = new Scanner(file);)
+        {
+            while (input.hasNext())
+            {
                 String firstName = input.next();
                 String middleNameInitial = input.next();
                 String lastName = input.next();
---
+++ src/exception_handling/ReformatCode.java
@@ -0,0 +1,61 @@
+package exception_handling;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReformatCode {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java ReformatCode Test.java");
+            System.exit(1);
+        }
+
+        String filename = args[0];
+
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("File specified does not exist");
+            System.exit(2);
+        }
+
+
+        try {
+            ArrayList<String> lines = new ArrayList<>();
+
+
+
+            try ( Scanner input = new Scanner(file);) {
+                while (input.hasNext()) {
+                    lines.add(input.nextLine());
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < lines.size(); i++) {
+                String currentLine = lines.get(i).trim();
+                String nextLine = ((i + 1) < lines.size()) ? lines.get(i + 1).trim() : "";
+
+
+                if (!currentLine.isEmpty() && nextLine.equals("{")) {
+                    result.append(lines.get(i)).append(" {");
+                    i++;
+                } else {
+                    result.append(lines.get(i));
+                }
+
+                if (i < lines.size() - 1) {
+                    result.append(System.lineSeparator());
+                }
+            }
+
+            try (PrintWriter writer = new PrintWriter(file)) {
+                writer.print(result.toString());
+            }
+
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
