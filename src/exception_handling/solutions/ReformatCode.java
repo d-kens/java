2754da1910b8e102bee779ea2d7f8e6c7eb540c2
d-kens <omondikens1@gmail.com>
2025-05-15 21:35:16 +0300

    Program to replace a string in a file with a new string for all files in the specified directory

--- src/exception_handling/solutions/ReformatCode.java
+++ src/exception_handling/solutions/ReformatCode.java
@@ -1,38 +1,39 @@
 package exception_handling.solutions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReformatCode {
+
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java ReformatCode Test.java");
             System.exit(1);
         }
 
-        String filename = args[0];
-
-        File file = new File(filename);
+        File file = new File(args[0]);
         if (!file.exists()) {
             System.out.println("File specified does not exist");
             System.exit(2);
         }
 
+        convertToEndLineBrace(file);
+    }
+
+    public static void convertToEndLineBrace(File file) {
+        ArrayList<String> lines = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
 
         try {
-            ArrayList<String> lines = new ArrayList<>();
-
-
-
-            try ( Scanner input = new Scanner(file);) {
+            try (Scanner input = new Scanner(file)) {
                 while (input.hasNext()) {
                     lines.add(input.nextLine());
                 }
             }
 
-            StringBuilder result = new StringBuilder();
             for (int i = 0; i < lines.size(); i++) {
                 String currentLine = lines.get(i).trim();
                 String nextLine = ((i + 1) < lines.size()) ? lines.get(i + 1).trim() : "";
@@ -54,8 +55,9 @@ public class ReformatCode {
                 writer.print(result.toString());
             }
 
-        } catch (Exception exception) {
+        } catch (FileNotFoundException exception) {
             System.out.println(exception.getMessage());
         }
     }
+
 }
---
+++ src/exception_handling/solutions/ReplaceText.java
@@ -0,0 +1,66 @@
+package exception_handling.solutions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ to replace a string in a file
+ with a new string for all files in the specified directory using the command:
+ java Exercise12_22 dir oldString newString
+ */
+
+public class ReplaceText {
+    public static void main(String[] args) {
+
+        if (args.length != 3) {
+            System.out.println(
+                    "Usage: java ReplaceText.java dir oldString newString"
+            );
+            System.exit(1);
+        }
+
+        String directoryName = args[0];
+        String oldString = args[1];
+        String newString = args[2];
+
+        File file = new File(directoryName);
+
+        if (!file.exists()) {
+            System.out.println("The file specified does not exist");
+            System.exit(2);
+        }
+
+        if(!file.isDirectory()) {
+            System.out.println("The file specified is not a directory");
+            System.exit(2);
+        }
+
+        File[] dirFiles = file.listFiles();
+
+        try {
+
+            for (File dirFile : dirFiles) {
+
+                StringBuilder output = new StringBuilder();
+
+                try (Scanner input = new Scanner(dirFile)) {
+                    while (input.hasNext()) {
+                        String line = input.nextLine();
+                        output.append(line.replace(oldString, newString)).append(System.lineSeparator());
+                    }
+                }
+
+                try (PrintWriter writer = new PrintWriter(dirFile)) {
+                    writer.print(output.toString());
+                }
+
+            }
+
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+
+    }
+}
