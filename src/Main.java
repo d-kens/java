14e45a6d595cf3155a7ca58f4c0a10bb6bd8fa15
d-kens <omondikens1@gmail.com>
2025-06-27 22:29:17 +0300

    Assignmenet statements, named constants, naming conventions

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final int SIZE = 20;
+        int count = 1;
+        double radius, interestRate;
 
+       int i, j, k;
+       i = j = k = 1; // if a value is to be assigned to multiple variables
 
+        double miles = 100;
+        final double KILOMETER_PER_MILE = 1.609;
 
+        double kilometers = miles * KILOMETER_PER_MILE;
 
+        System.out.println(kilometers);
     }
 
 }
