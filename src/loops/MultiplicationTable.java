6a5df27f186479c984d8c7758be3dae17bf356f5
d-kens <omondikens1@gmail.com>
2025-08-26 06:29:39 +0300

    multiplication table

---
+++ src/loops/MultiplicationTable.java
@@ -0,0 +1,25 @@
+package loops;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        System.out.println("Multiplication Table");
+
+        System.out.print("    ");
+        for (int i = 1; i <= 9; i++)
+            System.out.print("   " + i);
+
+        System.out.println("\n________________________________________");
+
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + " | ");
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
