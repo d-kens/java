8aa5c36496fd00509d5746961119188c9f915586
d-kens <omondikens1@gmail.com>
2025-08-27 15:45:42 +0300

    Convert decimal to hexdecimal

--- src/loops/Dec2Hex.java
+++ src/loops/Dec2Hex.java
@@ -6,18 +6,17 @@ public class Dec2Hex {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter a decimal number: ");
         int decimal = input.nextInt();
-
         String hex = "";
 
         while (decimal != 0) {
-            int  hexValue = decimal % 16;
+            int hexValue = decimal % 16;
 
-            // Convert the digit to decimal value
+            char hexDigit = (hexValue >= 0 && hexValue <= 9) ? (char)(hexValue + '0') : (char)(hexValue - 10 + 'A');
 
-
-            hex = hexValue + hex;
+            hex = hexDigit + hex;
 
             decimal /= 16;
         }
