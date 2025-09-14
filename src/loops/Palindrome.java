00defd7ffc372523c612d40de4db8327fa70afe7
d-kens <omondikens1@gmail.com>
2025-09-14 16:16:12 +0300

    Program to check if string is a palindrome'

--- src/loops/Palindrome.java
+++ src/loops/Palindrome.java
@@ -1,15 +1,45 @@
+
+/**
+ - A string is palindrome if it reads the same forward and backward.
+ - For example the words "mom", "dad", "noon" are all palindromes
+
+
+ Logical solution
+ - Check whether the first character in the string is same as the last character.
+ - If they are, check if the second character is the same as the second last character.
+ - This process continues until a mismatch is found or all the characters in the string are checked, except the middle character if the string has odd number of characters
+
+ - Variables low and high will store the index of the characters in the string that are being compared.
+ -
+
+ */
+
+
+
+
+
+
+
 package loops;
-
-import java.util.Scanner;
-
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter a string: ");
-        String string = input.nextLine();
+        String string = "noona";
+        int low = 0;
+        int high = string.length() - 1;
+        boolean isPalindrome = true;
 
+        while (low < high) {
+            if (string.charAt(low) != string.charAt(high)) {
+                isPalindrome = false;
+                break;
+            }
 
-     
+            low++;
+            high--;
+        }
+
+        String output = isPalindrome ? string + " is a palindrome." : string + " is not a palindrome";
+        System.out.println(output);
     }
 }
