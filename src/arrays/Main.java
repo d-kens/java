cb9e96e48ab7f235a350734f507323844f4b68ed
d-kens <omondikens1@gmail.com>
2026-01-15 06:28:15 +0300

    Simple arithmetic calculator using command line arguements

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -1,14 +1,62 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] source = {1, 5, 3, 6, 10, 21};
+        int[] numbers = {490, 65, 12, 9, 33, 64, 100};
 
-        int[] target = new int[4];
+        Arrays.parallelSort(numbers, 1, 4);
 
-        System.arraycopy(source, 1, target, 0, 4);
+        for (int number : numbers)
+            System.out.print(number + " ");
 
-        for (int value : target)
-            System.out.println(value);
+        System.out.println();
+        System.out.println();
+
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        System.out.println(Arrays.binarySearch(list, 1000));
+
+
+        int[] list1 = {2, 4, 7, 10};
+        int[] list2 = {2, 4, 7, 10};
+        int[] list3 = {4, 2, 7, 10};
+
+        System.out.println(Arrays.equals(list1, list2));
+        System.out.println(Arrays.equals(list2, list3));
+
+        System.out.println(Arrays.toString(list));
+
+        java.util.Arrays.fill(list1, 5);
+        java.util.Arrays.fill(list, 1, 5, 100);
+
+        for (int number : list1)
+            System.out.print(number + " ");
+
+        System.out.println();
+
+        for (int number : list)
+            System.out.print(number + " ");
+
+
+        String[] messages = {"Wewe ni nani?", "Na unatoka wapi?", "How are you", "Tulikutana wapi", "Abebo"};
+
+        Arrays.sort(messages);
+
+        System.out.println();
+        for (String message: messages)
+            System.out.println(message);
+
+
+
+        int[] list11 = {2, 4, 7, 10};
+        java.util.Arrays.fill(list11, 7);
+        System.out.println(java.util.Arrays.toString(list11));
+
+        int[] list12 = {2, 4, 7, 10};
+        System.out.println(java.util.Arrays.toString(list12));
+        System.out.print(java.util.Arrays.equals(list11, list12));
+
+
     }
 }
