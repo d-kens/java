603d24c11b73aefcd3416a81aae165b61dab5947
d-kens <omondikens1@gmail.com>
2026-05-09 15:32:41 +0300

    objects and classes

--- src/oop/Circle.java
+++ src/oop/Circle.java
@@ -3,6 +3,9 @@ package oop;
 public class Circle {
 
     public static void main(String[] args) {
+
+        System.out.println("Before creating any circle objects the number of circle object is: " + Circle.getNumberOfCircleObjects());
+
         // Create a circle with radius 1
         Circle circle1 = new Circle();
         System.out.println("The area of the circle with radius: " +
@@ -22,16 +25,35 @@ public class Circle {
         circle2.setRadius(100);
         System.out.println("The area of the circle with radius: " +
                 circle2.radius + " is: " + circle2.getArea());
+
+        System.out.println("After creating a couple of circle objects, the number of circle objects is: " + Circle.numberOfCircleObjects);
+
     }
 
-    double radius;
+    private double radius;
+
+    private static int numberOfCircleObjects = 0;
 
     public Circle() {
         radius = 1;
+        numberOfCircleObjects++;
     }
 
     public Circle(double newRadius) {
         radius = newRadius;
+        numberOfCircleObjects++;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double newRadius) {
+        radius = (newRadius > 0) ? newRadius : 0;
+    }
+
+    public static int getNumberOfCircleObjects() {
+        return numberOfCircleObjects;
     }
 
     double getArea() {
@@ -40,9 +62,5 @@ public class Circle {
 
     double getPerimeter() {
         return 2 * Math.PI * radius;
-    }
-
-    void setRadius(double newRadius) {
-        radius = newRadius;
     }
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -1,7 +1,57 @@
 package oop;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String [] args) {
+        Student student = new Student();
+        System.out.println("name: " + student.name);
+        System.out.println("age: " + student.age);
+        System.out.println("is science major: " + student.isScienceMajor);
+        System.out.println("gender: " + student.gender);
 
+        Date date = new Date();
+        System.out.println("The elapsed time since Jan 1, 1970 is " + date.getTime() + " milliseconds");
+        System.out.println(date.toString());
+
+
+        Circle[] circles = createCircleArray();
+        printCircleArray(circles);
     }
+
+
+    // Create an array of Circle Objects
+    public static Circle[] createCircleArray() {
+        Circle[] circles = new Circle[5];
+        for (int i = 0; i < circles.length; i++) {
+            circles[i] = new Circle(Math.random() * 100);
+        }
+        return circles;
+    }
+
+    public static void printCircleArray(Circle[] circles) {
+        System.out.printf("%-30s%-15s\n", "Radius", "Area");
+        for (int i = 0; i < circles.length; i++) {
+            System.out.printf("%-30f%-15f\n", circles[i].getRadius(),
+                    circles[i].getArea());
+        }
+        System.out.println("— — — — — — — — — — — — — — — — — — — — — — — — — — — —");
+        System.out.printf("%-30s%-15f\n", "The total area of circles is", sum(circles));
+    }
+
+    public static double sum(Circle[] circleArray) {
+        // Initialize sum
+        double sum = 0;
+        // Add areas to sum
+        for (int i = 0; i < circleArray.length; i++)
+            sum += circleArray[i].getArea();
+        return sum;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    boolean isScienceMajor;
+    char gender;
 }
