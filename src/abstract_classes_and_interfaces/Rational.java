2bff2e859aaabd812ddcb221c642682032c74689
d-kens <omondikens1@gmail.com>
2026-09-14 21:16:15 +0300

    Rational number

---
+++ src/abstract_classes_and_interfaces/Rational.java
@@ -0,0 +1,99 @@
+package abstract_classes_and_interfaces;
+
+/**
+ * Rational Number - Any number that can be written as a simple fraction where both the numerator and denominator are integers and the denominator is not zero.
+    - To reduce a rational number to its lowest terms, you need to find the GCD of the absolute values of its numerator and denominator, then divide numerator and denominator by this value
+ */
+
+public class Rational extends Number implements Comparable<Rational> {
+    private long numerator = 0;
+    private long denominator = 1;
+
+
+    public Rational() {
+        this(0, 1);
+    }
+
+    public Rational(long numerator, long denominator) {
+        long gcd = gcd(numerator, denominator);
+        this.numerator = (denominator > 0 ? 1 : -1) * numerator / gcd;
+        this.denominator = Math.abs(denominator) / gcd;
+    }
+
+    private static long gcd(long numerator, long denominator) {
+        long n1 = Math.abs(numerator);
+        long n2 = Math.abs(denominator);
+
+        int gcd = 1;
+
+        for (int k = 1; k <= n1 && k <= n2; k++) {
+            if (n1 % k == 0 && n2 % k == 0) {
+                gcd = k;
+            }
+        }
+
+        return gcd;
+    }
+
+    public long getNumerator() {
+        return numerator;
+    }
+
+    public long getDenominator() {
+        return denominator;
+    }
+
+    /** Add a rational number to this rational */
+    public Rational add(Rational secondRational) {
+        long n = numerator * secondRational.getDenominator() + denominator * secondRational.getNumerator();
+        long d = denominator * secondRational.getDenominator();
+        return new Rational(n, d);
+    }
+
+    /** Subtract a rational number from this rational */
+    public Rational subtract(Rational secondRational) {
+        long n = numerator * secondRational.getDenominator() - denominator * secondRational.getNumerator();
+        long d = denominator * secondRational.getDenominator();
+        return new Rational(n, d);
+    }
+
+    /** Multiply a rational number by this rational */
+    public Rational multiply(Rational secondRational) {
+        long n = numerator * secondRational.getNumerator();
+        long d = denominator * secondRational.getDenominator();
+        return new Rational(n, d);
+    }
+
+    /** Divide a rational number by this rational */
+    public Rational divide(Rational secondRational) {
+        long n = numerator * secondRational.getDenominator();
+        long d = denominator * secondRational.getNumerator();
+        return new Rational(n, d);
+    }
+
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Rational o) {
+        return 0;
+    }
+}
